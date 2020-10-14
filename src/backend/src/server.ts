2ef3e524f55410c3d584ae012436be4a3928aeb8
x0n4d0 <romagnolimauricio@gmail.com>
2020-10-14 18:11:41 -0300

    feat: add import to routes and app.use(routes)

--- src/backend/src/server.ts
+++ src/backend/src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import './database/connection';
+
+import { routes } from './routes';
+
 dotenv.config();
 
 const PORT = process.env.SERVER_APP_HAPPY_PORT;
@@ -8,6 +12,7 @@ const PORT = process.env.SERVER_APP_HAPPY_PORT;
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`[*] Server port: ${PORT}`)
