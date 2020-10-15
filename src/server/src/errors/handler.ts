3d29de996f0dbddd81d043a205b20b928cabe1eb
x0n4d0 <romagnolimauricio@gmail.com>
2020-10-15 19:50:43 -0300

    feat: add yup validation and error exceptions

--- src/server/src/errors/handler.ts
+++ src/server/src/errors/handler.ts
@@ -1,5 +1,19 @@
 import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
+
+import { ValidationErrorsInterface } from '../interfaces/ValidationErrorsInteface';
 
 export const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
+  if (error instanceof ValidationError) {
+    let errors: ValidationErrorsInterface = {};
+
+    error.inner.forEach(err => {
+      errors[err.path] = err.errors;
+    });
+  
+    return response.status(400).json({ message: 'Validation Fails', errors });
+  }
+
+  console.error(error);
   return response.status(500).json({ message: 'Internal Server Error' });
 };
---
+++ src/server/src/interfaces/ValidationErrorsInteface.ts
@@ -0,0 +1,3 @@
+export interface ValidationErrorsInterface {
+  [key: string]: string[];
+}
