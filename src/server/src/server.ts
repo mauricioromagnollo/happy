50288430eab95d15b9bea8e555df6315af4253b1
x0n4d0 <romagnolimauricio@gmail.com>
2020-10-15 19:31:22 -0300

    feat: add import to errorHandler

--- src/server/src/server.ts
+++ src/server/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
 import path from 'path';
+
 import _ from './config/env';
 import './database/connection';
 import { routes } from './routes';
+import { errorHandler } from './errors/handler';
 
 const PORT = process.env.NODE_APP_PORT;
 const app = express();
@@ -10,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`[*] Happy server running on port ${PORT}...`);
